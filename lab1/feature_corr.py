6b5433c917bb6cbba33327a324ca71bff33d2bb2
Chris Lin <chris4540@gmail.com>
2019-04-07 16:31:32 +0200

    Wrap update part 5

--- lab1/feature_corr.py
+++ lab1/feature_corr.py
@@ -1,28 +1,48 @@
 """
-For section 5
+Visualization code for part 5
+
+Q: Are features correlated?
+A: They are not correlated.
+
+Q: Is the assumption of diagonal covariance matrices for Gaussian modelling justified?
+A: From the graph we can see that the off-diagonal term are small and close to zero.
+   Therefore, the gaussian modelling is justified.
 """
 import numpy as np
 from lab1_proto import mfcc
+from lab1_proto import mspec
 import matplotlib.pyplot as plt
 
-def get_corrcoef_matrix(data):
-    all_features = np.empty((0,13))
+def get_corrcoef_matrix(data, feature="mfcc"):
+    assert feature in ["mfcc", "mspec"]
+    all_features = None
     for d in data:
         sample = d['samples']
         sampling_rate = d['samplingrate']
-        features = mfcc(sample)
-        all_features = np.concatenate((all_features, features), axis=0)
+        if feature == "mfcc":
+            features = mfcc(sample, samplingrate=sampling_rate)
+        elif feature == "mspec":
+            features = mspec(sample, samplingrate=sampling_rate)
+
+        if all_features is None:
+            all_features = features
+        else:
+            all_features = np.concatenate((all_features, features), axis=0)
 
     return np.corrcoef(all_features.T)
 
 if __name__ == "__main__":
     data = np.load('data/lab1_data.npz')['data']
-    corr = get_corrcoef_matrix(data)
 
-    # plot the matrix out
-    fig, ax = plt.subplots()
-    cax = ax.matshow(corr)
-    fig.colorbar(cax)
-    ax.xaxis.tick_bottom()
-    plt.title("Correlation coefficient matrix")
-    plt.savefig("part5_cof_mat.png")
+    for f in ["mfcc", "mspec"]:
+        corr = get_corrcoef_matrix(data, feature=f)
+        # plot the matrix out
+        fig, ax = plt.subplots()
+        cax = ax.matshow(corr)
+        fig.colorbar(cax)
+        ax.xaxis.tick_bottom()
+        plt.title("Correlation coefficient matrix of %s" % f)
+        plt.tight_layout()
+        plt.savefig("part5_%s_cof_mat.png" % f)
+
+
