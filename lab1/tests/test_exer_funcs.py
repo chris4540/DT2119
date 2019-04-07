1cd28bf9aafe3003a89619838d0fbc8dac237e9d
Chris Lin <chris4540@gmail.com>
2019-04-07 22:26:12 +0200

    Update the test assertion tolerance params

--- lab1/tests/test_exer_funcs.py
+++ lab1/tests/test_exer_funcs.py
@@ -25,34 +25,34 @@ class TestExerciseFunctions(unittest.TestCase):
         winshift = int(self.win_shift_s * sampling_rate)
         frames = enframe(self.samples, winlen=winlen, winshift=winshift)
         exp_ans = self.data['frames']
-        assert_allclose(frames, exp_ans, rtol=0, atol=0)
+        assert_allclose(frames, exp_ans)
 
     def test_preemp(self):
         # frames = enframe(self.samples, winlen=self.winlen, winshift=self.winshift)
         pre_emph = preemp(self.data['frames'], p=0.97)
         exp_ans = self.data['preemph']
-        assert_allclose(pre_emph, exp_ans, rtol=0, atol=0)
+        assert_allclose(pre_emph, exp_ans)
 
     def test_windowing(self):
         windowed = windowing(self.data['preemph'])
         exp_ans = self.data['windowed']
-        assert_allclose(windowed, self.data['windowed'], rtol=0, atol=0)
+        assert_allclose(windowed, self.data['windowed'])
 
     def test_powerSpectrum(self):
         exp_ans = self.data['spec']
         nfft = exp_ans.shape[1]
         power_spec = powerSpectrum(self.data['windowed'], nfft=nfft)
-        assert_allclose(power_spec, exp_ans, rtol=0, atol=0)
+        assert_allclose(power_spec, exp_ans)
 
     def test_logMelSpectrum(self):
         mspec = logMelSpectrum(self.data['spec'], self.data['samplingrate'])
         exp_ans = self.data['mspec']
-        assert_allclose(mspec, exp_ans, rtol=0, atol=0)
+        assert_allclose(mspec, exp_ans)
 
     def test_cepstrum(self):
         mfcc = cepstrum(self.data['mspec'], nceps=self.nceps)
-        assert_allclose(mfcc, self.data['mfcc'], rtol=0, atol=0)
+        assert_allclose(mfcc, self.data['mfcc'])
 
     def test_lmfcc(self):
         lmfcc = lifter(self.data['mfcc'])
-        assert_allclose(lmfcc, self.data['lmfcc'], rtol=0, atol=0)
+        assert_allclose(lmfcc, self.data['lmfcc'])
