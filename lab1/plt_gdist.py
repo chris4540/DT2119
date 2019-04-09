bb6d9c09da9daac682dae4f6d64c1857ba81181f
Chris Lin <chris4540@gmail.com>
2019-04-09 11:39:07 +0200

    Add plotting the global distance matrix

--- lab1/plt_gdist.py
+++ lab1/plt_gdist.py
@@ -7,15 +7,24 @@ from scipy.cluster.hierarchy import dendrogram
 import config
 
 if __name__ == '__main__':
+    # mkdir -p plots
     try:
         os.makedirs("plots")
     except FileExistsError:
         pass
 
+    # load data files
     gdist =  np.load(config.gdist_npy_file)
     data = np.load(config.lab1_npz_file)['data']
-    # plt.matshow(mat)
 
+    # plot the matrix
+    fig, ax = plt.subplots(1, 1)
+    cax = ax.matshow(gdist)
+    fig.colorbar(cax)
+    plt.savefig('plots/gdist_mat.png', dpi=200, bbox_inches='tight')
+
+
+    # plot the dendrogram
     labels = tidigit2labels(data)
     fig = plt.figure(figsize=(25, 10))
     Z = linkage(gdist, "complete")
