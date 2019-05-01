824e60509ff9b39694d76a2af528644df994daad
Chris Lin <chris4540@gmail.com>
2019-05-01 11:51:01 +0200

    Add prondict

---
+++ lab2/prondict.py
@@ -0,0 +1,19 @@
+prondict = {}
+prondict['o'] = ['ow']
+prondict['z'] = ['z', 'iy', 'r', 'ow']  # zero
+prondict['1'] = ['w', 'ah', 'n']
+prondict['2'] = ['t', 'uw']
+prondict['3'] = ['th', 'r', 'iy']
+prondict['4'] = ['f', 'ao', 'r']
+prondict['5'] = ['f', 'ay', 'v']
+prondict['6'] = ['s', 'ih', 'k', 's']
+prondict['7'] = ['s', 'eh', 'v', 'ah', 'n']
+prondict['8'] = ['ey', 't']
+prondict['9'] = ['n', 'ay', 'n']
+
+# ====================================================
+# build the isolated word prondict
+isolated = dict()
+for d in prondict.keys():
+    isolated[d] = ['sil', *prondict[d], 'sil']
+# ====================================================
