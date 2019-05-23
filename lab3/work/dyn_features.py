e9ef58a89381cd767e3e76f8c0a79c3b42d2cab1
Chris Lin <chris4540@gmail.com>
2019-05-23 23:02:15 +0200

    Add building Dynamic Features script

--- lab3/work/dyn_features.py
+++ lab3/work/dyn_features.py
@@ -1,7 +1,8 @@
 """
-Script to build dynamic features
+Script to build dynamic features + normalization
 """
 import numpy as np
+from tqdm import tqdm
 
 def stack_features(feature_mat, stack=3):
     """
@@ -23,8 +24,60 @@ def stack_features(feature_mat, stack=3):
         stack_list.append(padded_features[start_idx:end_idx])
 
     stacked_mat = np.hstack(stack_list)
-
     return stacked_mat
 
 if __name__ == "__main__":
-    pass
+    feature_name = 'lmfcc'
+    # feature_name = 'mspec'
+    print("[Dynamic feature] Working on the feature: ", feature_name)
+    # load data
+    data = np.load('data/train_val_data.npz')
+    valdata = data['validation']
+    traindata = data['train']
+    testdata = np.load('data/testdata.npz')['testdata']
+
+    # Calculate the mean and std first
+    print("Calculate the mean and std ....")
+    # for each utterance generate dynamic features
+    train_features = []
+    for d in tqdm(traindata):
+        feature_mat = d[feature_name]
+        stacked = stack_features(feature_mat)
+        train_features.append(stacked)
+
+    train_concat = np.concatenate(train_features, axis=0)
+    # cal mean and std
+    mean = np.mean(train_concat, axis=0)
+    std = np.std(train_concat,  axis=0)
+    print("Complete calculate the mean and std")
+    train_concat = None
+    train_features = None
+    # =========================================================
+    datasets = {
+        'train': traindata,
+        'test': testdata,
+        'val': valdata,
+    }
+
+    for k, v in datasets.items():
+        data = list()
+        for d in tqdm(v):
+            new_data = dict()
+            new_data['filename'] = d['filename']
+            new_data['targets'] = d['targets']
+            dyn_feature = stack_features(d[feature_name])
+            new_data[feature_name] = (dyn_feature - mean) / std
+            data.append(new_data)
+
+        print("[Dynamic feature]Complete normlaizating ", k)
+        # save it
+        np.savez('data/dynamic/d{}_{}.npz'.format(feature_name, k), data=data)
+        print("[Dynamic feature] Wrote the normlaized data", k)
+
+
+
+
+
+
+
+
