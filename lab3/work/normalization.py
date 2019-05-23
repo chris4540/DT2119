e5870b9ded5e5612ea3c595bcd7a487a2ac8cc63
Chris Lin <chris4540@gmail.com>
2019-05-23 23:05:18 +0200

    fix format of normalization

--- lab3/work/normalization.py
+++ lab3/work/normalization.py
@@ -3,6 +3,8 @@ Data normalization for 4.6
 """
 import numpy as np
 from tqdm import tqdm
+
+
 def get_mean_std(data, feature):
    # Obtain all data
    all_X = list()
@@ -17,36 +19,36 @@ def get_mean_std(data, feature):
 
 
 if __name__ == "__main__":
-   # feature_name = 'lmfcc'
-   feature_name = 'mspec'
-   print("Working on the feature: ", feature_name)
-   # load data
-   data = np.load('data/train_val_data.npz')
-   valdata = data['validation']
-   traindata = data['train']
-   testdata = np.load('data/testdata.npz')['testdata']
+    # feature_name = 'lmfcc'
+    feature_name = 'mspec'
+    print("Working on the feature: ", feature_name)
+    # load data
+    data = np.load('data/train_val_data.npz')
+    valdata = data['validation']
+    traindata = data['train']
+    testdata = np.load('data/testdata.npz')['testdata']
 
-   # calculate the mean and std from train data
-   mean, std = get_mean_std(traindata, feature_name)
-   print("Complete calculating mean and std")
-   datasets = {
-      'train': traindata,
-      'test': testdata,
-      'val': valdata,
-   }
+    # calculate the mean and std from train data
+    mean, std = get_mean_std(traindata, feature_name)
+    print("Complete calculating mean and std")
+    datasets = {
+        'train': traindata,
+        'test': testdata,
+        'val': valdata,
+    }
 
-   for k, v in datasets.items():
-      data = list()
-      for d in tqdm(v):
-         new_data = dict()
-         new_data['filename'] = d['filename']
-         new_data['targets'] = d['targets']
-         new_data[feature_name] = (d[feature_name] - mean) / std
-         data.append(new_data)
+    for k, v in datasets.items():
+        data = list()
+        for d in tqdm(v):
+            new_data = dict()
+            new_data['filename'] = d['filename']
+            new_data['targets'] = d['targets']
+            new_data[feature_name] = (d[feature_name] - mean) / std
+            data.append(new_data)
 
-      print("Complete normlaizating ", k)
-      # save it
-      np.savez('data/nondynamic/{}_{}.npz'.format(feature_name, k), data=data)
+        print("Complete normlaizating ", k)
+        # save it
+        np.savez('data/nondynamic/{}_{}.npz'.format(feature_name, k), data=data)
 
 
 
