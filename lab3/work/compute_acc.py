bc9d55a4df8679bdc34592638e4fc8f3cb9639a5
Chris Lin <chris4540@gmail.com>
2019-05-24 10:23:56 +0200

    Add results

--- lab3/work/compute_acc.py
+++ lab3/work/compute_acc.py
@@ -9,6 +9,50 @@ For windows:
 import numpy as np
 import Levenshtein
 from tqdm import tqdm
+import json
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+
+
+def plot_confusion_matrix(y_true, y_pred,
+                          normalize=True,
+                          title=None,
+                          cmap=plt.cm.viridis):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+    # Only use the labels that appear in the data
+    # classes = classes[unique_labels(y_true, y_pred)]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(
+        # xticks=np.arange(cm.shape[1]),
+        # yticks=np.arange(cm.shape[0]),
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    fig.tight_layout()
+    return ax
 
 def list_idx_to_str(list_):
     ret = ''.join(str(x) for x in list_)
@@ -19,24 +63,6 @@ def levenshtein_dist(s, t):
     t_string = list_idx_to_str(t)
     ret = Levenshtein.distance(s_string, t_string)
     return ret
-
-# def levenshtein_dist(s, t):
-#     if not s: # consider the empty list
-#         return len(t)
-#     if not t:  # consider the empty list
-#         return len(s)
-
-#     # compare the last components
-#     if s[-1] == t[-1]:
-#         cost = 0
-#     else:
-#         cost = 1
-
-#     # pop out the last one, compare recursively by dynamic programming
-#     res = min([levenshtein_dist(s[:-1], t)+1,
-#                levenshtein_dist(s, t[:-1])+1,
-#                levenshtein_dist(s[:-1], t[:-1]) + cost])
-#     return res
 
 def parse_data(data, feature):
     """
@@ -66,9 +92,11 @@ def merge_conseq_ident_seq(seq):
     return ret
 
 
+
 if __name__ == "__main__":
     feature = "lmfcc"
     # feature = "mspec"
+    # dynamic = True
     dynamic = False
     # =========================================================
     if dynamic:
@@ -87,6 +115,10 @@ if __name__ == "__main__":
     pred_y = np.argmax(test_class_prob, axis=1)
     test_acc = (pred_y == test_y).mean()
     print("[{}] Frame-by-frame acc. = ".format(tag), test_acc)
+    ax = plot_confusion_matrix(test_y, pred_y)
+    ax.set_title("Confusion matrix at state level")
+    fig = ax.get_figure()
+    fig.savefig("{}_state_cm.png".format(tag), bbox_inches='tight')
     # ==============================================================
     # compute the avg. edit distance
     cnt = 0
@@ -105,12 +137,48 @@ if __name__ == "__main__":
 
     print("[{}] Frame-by-frame edit dist avg. = ".format(tag), np.mean(edit_dists))
 
+    # =============================================================
+    # build mapping from state to phone (index)
+    with open("data/state_list.json", 'r') as f:
+        state_list = json.load(f)['state_list']
 
 
-
-
-    # # 1. merging
-    # pred_y_merged = merge_conseq_ident_seq(pred_y)
-    # test_y_merged = merge_conseq_ident_seq(test_y)
-    # edit_dist = levenshtein_dist(pred_y_merged, test_y_merged)
-    # print(edit_dist)
+    map_state_to_phone_idx = dict()
+    phone_idx = dict()
+    for i, s in enumerate(state_list):
+        phone = s.split("_")[0]
+        if phone not in phone_idx:
+            phone_idx[phone] = len(phone_idx)
+        # ====================================
+        map_state_to_phone_idx[i] = phone_idx[phone]
+    # ================================================================
+    # frame-by-frame at the phoneme level
+    v_map = np.vectorize(map_state_to_phone_idx.get)
+    pred_y_phone = v_map(pred_y)
+    test_y_phone = v_map(test_y)
+    test_acc = (pred_y_phone == test_y_phone).mean()
+    print("[{}] Frame-by-frame acc. at the phoneme level = ".format(tag), test_acc)
+    # =================================================================
+    ax = plot_confusion_matrix(test_y_phone, pred_y_phone)
+    ax.set_title("Confusion matrix at phoneme level")
+    fig = ax.get_figure()
+    fig.savefig("{}_phone_cm.png".format(tag), bbox_inches='tight')
+    # ==============================================================
+    # compute the avg. edit distance
+    cnt = 0
+    edit_dists = list()
+    for d in tqdm(test_data):
+        targets = d['targets']
+        targets = v_map(targets)
+        end_idx = cnt + len(targets)
+        y_pred_this = pred_y[cnt:end_idx]
+        # mapping
+        y_pred_this = v_map(y_pred_this)
+        target_merged = merge_conseq_ident_seq(targets)
+        y_merged = merge_conseq_ident_seq(y_pred_this)
+        dist = levenshtein_dist(target_merged, y_merged)
+        dist /= max(len(target_merged), len(y_merged))
+        edit_dists.append(dist)
+        #
+        cnt = end_idx
+    print("[{}] Frame-by-frame edit dist avg. at the phoneme level = ".format(tag), np.mean(edit_dists))
